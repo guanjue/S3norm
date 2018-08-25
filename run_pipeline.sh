0a9248a9d939d7f94819596dee4e772277aa661b
Universe <universe@MacBook-Air.local>
2018-08-25 00:05:38 -0400

    adjust plot num & rank num

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -7,7 +7,7 @@ overall_upper=16
 overall_lower=2
 select_method=frip
 select_ref_version=median
-user_given_global_ref=ERY_fl.h3k4me3rep.fisher_p.txt
+user_given_global_ref=NA
 bin_num=13554672
 
 time bash $script_dir'overall_pipeline.sh' $script_dir $working_dir $input_dir $input_file_list $overall_upper $overall_lower $select_method $select_ref_version $user_given_global_ref $bin_num
