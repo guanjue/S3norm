6c92a8bd781c8f9dc52b62aba66d1754b64cfb2c
Universe <universe@MacBook-Air.local>
2019-05-06 16:51:56 -0400

    ok

--- src/negative_binomial_p_2r_bgadj_bayes.R
+++ src/negative_binomial_p_2r_bgadj_bayes.R
@@ -189,10 +189,6 @@ nb_pval = apply(sig, MARGIN=1, function(x) get_pval(x[1], bin_num, sig_0_size, s
 
 ### get -log10(p-value)
 nb_pval[nb_pval<=1e-324] = 1e-324
-neglog10_nb_pval = -log10(nb_pval)
-
-### write output
-#write.table(neglog10_nb_pval, paste(output_name, '.nbp_1r_bgadj.txt', sep=''), quote=FALSE, col.names=FALSE, row.names=FALSE, sep='\t')
 
 ### get -log10(p-value)
 print('get -log10(p-value)')
