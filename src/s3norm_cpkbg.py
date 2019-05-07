abafd91b4d9f18862a2ef6baad2a83f9d84ae853
Universe <universe@MacBook-Air.local>
2019-05-06 23:45:36 -0400

    user given cpkbg

--- src/s3norm_cpkbg.py
+++ src/s3norm_cpkbg.py
@@ -134,78 +134,78 @@ def s3norm(sig1_wg_raw, sig2_wg_raw, moment, B_init, fdr_thresh, sample_num, ran
 	sig2[sig2<lowerlim] = lowerlim
 
 	### read whole genome binary label
+	if p_method == 'z':
+		#sig1_log2 = np.log2(sig1+0.01)
+		#sig1_z_p_fdr = p_adjust(1 - norm.cdf((sig1_log2 - np.mean(sig1_log2))/ np.std(sig1_log2)), 'fdr')
+		sig1a = sig1+0.01
+		sig1_z_p_fdr = p_adjust(1 - norm.cdf((sig1a - np.mean(sig1a))/ np.std(sig1a)), 'fdr')
+		sig1_binary = sig1_z_p_fdr < fdr_thresh
+	elif p_method == 't':
+		sig1_binary = sig1 >= np.quantile(sig1, fdr_thresh)
+	elif p_method == 'p':
+		sig1_p = 10.0**(-sig1)
+		sig1_z_p_fdr = p_adjust(sig1_p, 'fdr')
+		write2d_array(sig1_z_p_fdr.reshape(sig1_z_p_fdr.shape[0],1), sig1_wg_raw + '.nbp.txt')
+		sig1_binary = (sig1_z_p_fdr < fdr_thresh)
+
+	sig1_pk_num = np.sum(sig1_binary)
+	print('sig1_pk_num')
+	print(sig1_pk_num)
+
+	### if pk number < 10000 then use rank for sig2
+	if sig1_pk_num <= rank_lim:
+		sig1_thresh = np.sort(sig1, axis=None)[-rank_lim]
+		print('rank sig1')
+		sig1_binary = sig1 > sig1_thresh
+
+	print('sig1_pk_num')
+	print(sum(sig1_binary))
+	print(sig1_pk_num)
+
+	if p_method == 'z':
+		#sig2_log2 = np.log2(sig2+0.01)
+		#sig2_z_p_fdr = p_adjust(1 - norm.cdf((sig2_log2 - np.mean(sig2_log2))/ np.std(sig2_log2)), 'fdr')
+		sig2a = sig2+0.01
+		sig2_z_p_fdr = p_adjust(1 - norm.cdf((sig2a - np.mean(sig2a))/ np.std(sig2a)), 'fdr')
+		sig2_binary = sig2_z_p_fdr < fdr_thresh
+	elif p_method == 't':
+		sig2_binary = sig2 >= np.quantile(sig2, fdr_thresh)
+	elif p_method == 'p':
+		sig2_p = 10.0**(-sig2)
+		sig2_z_p_fdr = p_adjust(sig2_p, 'fdr')
+		write2d_array(sig2_z_p_fdr.reshape(sig2_z_p_fdr.shape[0],1), sig2_wg_raw + '.nbp.txt')
+		sig2_binary = (sig2_z_p_fdr < fdr_thresh)
+
+	sig2_pk_num = np.sum(sig2_binary)
+	print('sig2_pk_num')
+	print(sig2_pk_num)
+
+	### if pk number < 10000 then use rank for sig2
+	if sig2_pk_num <= rank_lim:
+		sig2_thresh = np.sort(sig2, axis=None)[-rank_lim]
+		print('rank sig2')
+		sig2_binary = sig2 > sig2_thresh
+
+	print('sig2_pk_num')
+	print(sum(sig2_binary))
+	print(sig2_pk_num)
+
 	if (common_pk_binary!='0') and (common_bg_binary!='0'):
 		common_pk_binary_sig = read2d_array(common_pk_binary, float)
 		peak_binary = (common_pk_binary_sig == 1.0)[:,0]
 		common_bg_binary_sig = read2d_array(common_bg_binary, float)
 		bg_binary = (common_bg_binary_sig == 0.0)[:,0]
+
+	### peak region (both != 0 in sig1 & sig2)
+	if ref == 'F':
+		peak_binary = (sig1_binary[:,0] & sig2_binary[:,0])
 	else:
-		if p_method == 'z':
-			#sig1_log2 = np.log2(sig1+0.01)
-			#sig1_z_p_fdr = p_adjust(1 - norm.cdf((sig1_log2 - np.mean(sig1_log2))/ np.std(sig1_log2)), 'fdr')
-			sig1a = sig1+0.01
-			sig1_z_p_fdr = p_adjust(1 - norm.cdf((sig1a - np.mean(sig1a))/ np.std(sig1a)), 'fdr')
-			sig1_binary = sig1_z_p_fdr < fdr_thresh
-		elif p_method == 't':
-			sig1_binary = sig1 >= np.quantile(sig1, fdr_thresh)
-		elif p_method == 'p':
-			sig1_p = 10.0**(-sig1)
-			sig1_z_p_fdr = p_adjust(sig1_p, 'fdr')
-			write2d_array(sig1_z_p_fdr.reshape(sig1_z_p_fdr.shape[0],1), sig1_wg_raw + '.nbp.txt')
-			sig1_binary = (sig1_z_p_fdr < fdr_thresh)
+		peak_binary = (sig1_binary[:,0] | sig2_binary[:,0])
+	print(np.sum(peak_binary))
 
-		sig1_pk_num = np.sum(sig1_binary)
-		print('sig1_pk_num')
-		print(sig1_pk_num)
-
-		### if pk number < 10000 then use rank for sig2
-		if sig1_pk_num <= rank_lim:
-			sig1_thresh = np.sort(sig1, axis=None)[-rank_lim]
-			print('rank sig1')
-			sig1_binary = sig1 > sig1_thresh
-
-		print('sig1_pk_num')
-		print(sum(sig1_binary))
-		print(sig1_pk_num)
-
-		if p_method == 'z':
-			#sig2_log2 = np.log2(sig2+0.01)
-			#sig2_z_p_fdr = p_adjust(1 - norm.cdf((sig2_log2 - np.mean(sig2_log2))/ np.std(sig2_log2)), 'fdr')
-			sig2a = sig2+0.01
-			sig2_z_p_fdr = p_adjust(1 - norm.cdf((sig2a - np.mean(sig2a))/ np.std(sig2a)), 'fdr')
-			sig2_binary = sig2_z_p_fdr < fdr_thresh
-		elif p_method == 't':
-			sig2_binary = sig2 >= np.quantile(sig2, fdr_thresh)
-		elif p_method == 'p':
-			sig2_p = 10.0**(-sig2)
-			sig2_z_p_fdr = p_adjust(sig2_p, 'fdr')
-			write2d_array(sig2_z_p_fdr.reshape(sig2_z_p_fdr.shape[0],1), sig2_wg_raw + '.nbp.txt')
-			sig2_binary = (sig2_z_p_fdr < fdr_thresh)
-
-		sig2_pk_num = np.sum(sig2_binary)
-		print('sig2_pk_num')
-		print(sig2_pk_num)
-
-		### if pk number < 10000 then use rank for sig2
-		if sig2_pk_num <= rank_lim:
-			sig2_thresh = np.sort(sig2, axis=None)[-rank_lim]
-			print('rank sig2')
-			sig2_binary = sig2 > sig2_thresh
-
-		print('sig2_pk_num')
-		print(sum(sig2_binary))
-		print(sig2_pk_num)
-
-		### peak region (both != 0 in sig1 & sig2)
-		if ref == 'F':
-			peak_binary = (sig1_binary[:,0] & sig2_binary[:,0])
-		else:
-			peak_binary = (sig1_binary[:,0] | sig2_binary[:,0])
-		print(np.sum(peak_binary))
-
-		### background region (both == 0 in sig1 & sig2)
-		bg_binary = ~(sig1_binary[:,0] | sig2_binary[:,0])
-		print(np.sum(bg_binary))
+	### background region (both == 0 in sig1 & sig2)
+	bg_binary = ~(sig1_binary[:,0] | sig2_binary[:,0])
+	print(np.sum(bg_binary))
 
 	### get common bg pk
 	sig1_cbg = sig1[bg_binary,0]
