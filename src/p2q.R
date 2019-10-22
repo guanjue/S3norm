2047089f73d56b66e4de8c04c0ba3036376afde4
Universe <universe@MacBook-Air.local>
2019-10-22 12:30:30 -0400

    -log10

--- src/p2q.R
+++ src/p2q.R
@@ -7,7 +7,7 @@ method = args[3]
 
 d = read.table(pval_bedgraph_file, header=F)
 p = 10^(-d[,4])
-q = p.adjust(p, method)
+q = -log10(p.adjust(p, method))
 dq = cbind(d[,1:3], q)
 
 write.table(dq, qval_bedgraph_file, quote=F, col.names=F, row.names=F, sep='\t')
